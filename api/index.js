d9238755bdab42c394868324e52ceb27a946f211
webvss <vjat7466@gmail.com>
2026-05-21 10:05:14 +0530

    solve page not found error

--- api/index.js
+++ api/index.js
@@ -1,9 +1,3 @@
 const { createApp } = require('../src/app');
-const { initDb } = require('../src/config/database');
-
 const app = createApp();
-
-module.exports = async (req, res) => {
-  await initDb();
-  return app(req, res);
-};
+module.exports = app;
--- src/app.js
+++ src/app.js
@@ -4,8 +4,20 @@ const session = require('express-session');
 const authRoutes = require('./routes/authRoutes');
 const pageRoutes = require('./routes/pageRoutes');
 
+const { initDb } = require('./config/database');
+
 function createApp() {
   const app = express();
+
+  app.use(async (req, res, next) => {
+    try {
+      await initDb();
+      next();
+    } catch (err) {
+      console.error('Database connection failed:', err);
+      res.status(500).send('Database connection error');
+    }
+  });
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static(path.join(__dirname, '..', 'public')));
